2b6f44f7feeffdb8d12d3d2320c18b2092f7c70f
Eugene Eeo <packwolf58@gmail.com>
2015-07-31 23:36:48 +0800

    Add Scanner object and tests

--- katana/expr.py
+++ katana/expr.py
@@ -4,7 +4,23 @@ import re
 class Expr(object):
     def __init__(self, name, regex):
         self.name = name
-        self.regex = re.compile(regex)
+        self.regex = regex
 
     def on_match(self, string):
         return [self.name, string]
+
+    def scanner_callback(self, _, string):
+        return self.on_match(string)
+
+
+class Scanner(object):
+    def __init__(self, exprs):
+        self.scanner = re.Scanner([
+            (e.regex, e.scanner_callback) for e in exprs
+        ])
+
+    def match(self, string):
+        tokens, extra = self.scanner.scan(string)
+        if extra:
+            raise ValueError
+        return tokens
--- tests/test_expr.py
+++ tests/test_expr.py
@@ -1,5 +1,5 @@
 import pytest
-from katana.expr import Expr
+from katana.expr import Expr, Scanner
 
 
 @pytest.fixture
@@ -10,3 +10,21 @@ def expr():
 def test_on_match(expr):
     assert expr.on_match('t') == ['dollar', 't']
     assert expr.on_match('$') == ['dollar', '$']
+
+
+@pytest.fixture
+def scanner():
+    return Scanner([
+        Expr('dollar', r'\$'),
+        Expr('number', r'[0-9]+'),
+    ])
+
+
+def test_scanner_match_full(scanner):
+    assert scanner.match('$123') == [['dollar', '$'], ['number', '123']]
+    assert scanner.match('123') == [['number', '123']]
+
+
+def test_scanner_match_partial(scanner):
+    with pytest.raises(ValueError):
+        scanner.match('abc')

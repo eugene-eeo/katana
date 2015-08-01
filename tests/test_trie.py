8b25d2c67b23d02f9251855c32c15af557480c85
Eugene Eeo <packwolf58@gmail.com>
2015-08-01 11:44:03 +0800

    Test Trie.insert method

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -21,3 +21,8 @@ def test_trie_pos(trie):
     assert trie.pos('12') == ls('12', '123', '1234')
     assert trie.pos('123') == ls('123', '1234')
     assert trie.pos('1234') == ls('1234')
+
+
+def test_trie_insert(trie):
+    trie.insert('456')
+    assert trie.pos('456') == ls('456')

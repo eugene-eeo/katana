a24983a6540ad50e096fdee11826b077ba65332a
Eugene Eeo <packwolf58@gmail.com>
2015-08-28 23:00:11 +0800

    Test utils.parse function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,24 @@
+import pytest
 from pyrsistent import v as Vec
-from katana.utils import prepare, parse, Pair
+from katana.utils import prepare, parse, Pair, Node
+from katana.term import term
 
 
 def test_prepare():
     given = ['k']
     after = Pair(Vec(), Vec(*given))
     assert prepare(given) == after
+
+
+def test_parse():
+    node = Node('$', '123')
+    given = [node]
+    after = [node]
+    assert parse(term('$'), given) == after
+
+
+def test_parse_partial():
+    node = Node('$', '123')
+    given = [node, node]
+    with pytest.raises(ValueError):
+        parse(term('$'), given)
